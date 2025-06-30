d90151f306a812f275d7866212dd0aeb01bfac7d
Xiaochi Zhou <xz378@cam.ac.uk>
2025-06-30 11:58:55 +0100

    dev: updated TaskDecompositionAgent with different model and more detailed prompt
    - gpt-4o-mini is replaced with gpt-4o for task decompostion
    - More detailed prompt to guide the agent

--- src/agents/TaskDecompositionAgent.py
+++ src/agents/TaskDecompositionAgent.py
@@ -19,18 +19,18 @@ INSTRUCTION_PROMPT_GOLD = """
 
 I have a gaussian output log file at /data/test/benzene.log. This file contains the output of a gaussian calculation. 
 
-My system, the world avatar, uses semantic technologies to represent the data and support queries across data from different domains. More information can be found with "stack_operations" tool. 
-
-In this turn, I want you to integrate the data from the gaussian log file into my system. 
+In this turn, I want you to integrate the data from the gaussian log file into my system stack.  
 
 **Important**: In this turn, you don't need to actually do the work and give me the detailed plan only, taking consideration of the tools you have access to.
 
 Keep in mind a strategy that: 
 
 1. Read the tool descriptions carefully, where you can learn how the underlying system works. 
-2. Don't be afraid to include hypothetical tools, which will be created for the tasks
-3. Use tools instead of using LLMs to deal with files.
-4. Break the tasks in to as small as possible subtasks, each subtask should be yield a single task file. 
+2. Use as many existing tools as possible, as existing tools are more likely to be compatible with the existing system. 
+3. Don't be afraid to include hypothetical tools, which will be created for the tasks
+4. Use tools instead of using LLMs to deal with files, including data parsing and data extraction. If the tools are not available, you can propose to create them. 
+5. Break the tasks in to as small as possible subtasks, each subtask should be yield a single task file. 
+6. The planning must be compatible with existing systems. 
 
 
 Create task files with task_generation tool. 
@@ -41,7 +41,7 @@ Create task files with task_generation tool.
 async def main():
     model_config = ModelConfig()
     mcp_tools = ["all"]
-    agent = BaseAgent(model_name="gpt-4o-mini", model_config=model_config, remote_model=True, mcp_tools=mcp_tools)
+    agent = BaseAgent(model_name="gpt-4o", model_config=model_config, remote_model=True, mcp_tools=mcp_tools)
     response, metadata = await agent.run(INSTRUCTION_PROMPT_GOLD, recursion_limit=200)
     print(response)
  
